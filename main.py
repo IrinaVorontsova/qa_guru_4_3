f18f6d36cf5a79e16f0b2799593de4d37ef2b8a6
Irina Vorontsova <irina.n.vorontsova@gmail.com>
2023-03-08 19:32:02 +0300

    1

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ def test_one():
 def test_two(x, y):
     assert x > y, f"{y} больше {x}"
 
+def test_three(z):
+    assert z == 0, f"{z} не равно 0"
+
 
 print(test_two(2, 1))
 print(test_two(1, 2))
